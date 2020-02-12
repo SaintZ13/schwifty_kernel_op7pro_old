4efd1ad1b08089b5c1e8f0a83c726f87131561ca
Tyler Nijmeh <tylernij@gmail.com>
2020-02-11 22:09:41 -0800

    sched: Align scheduling periods with timer rate
    It is not possible to balance any sooner than the jiffy time in
    milliseconds, as the scheduler requires a timer interrupt (tick) to
    perform load balancing. We can make the scheduler as precise as possible
    within the constraint of our timer rate to reduce as much latency as
    possible.
    
    By aligning the scheduling period, we load balance every tick. For a 100
    Hz timer, we will be able to call the scheduler every 10 ms, and grant
    1ms per task at least.
    
    With a 1000 Hz timer, we will be balancing once every millisecond, which
    may result in poor throughput, but the latency will be at most 1 ms.
    
    THIS COMMIT IS EXPERIMENTAL. I highly suggest considering if your
    device has the processing power to perform in latencies under your timer
    rate in milliseconds.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -95,8 +95,8 @@ walt_dec_cfs_rq_stats(struct cfs_rq *cfs_rq, struct task_struct *p) {}
  *
  * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_latency			= 5000000ULL;
-unsigned int normalized_sysctl_sched_latency		= 5000000ULL;
+unsigned int sysctl_sched_latency			= (1000 / HZ) * 1000000ULL;
+unsigned int normalized_sysctl_sched_latency		= (1000 / HZ) * 1000000ULL;
 
 /*
  * Enable/disable honoring sync flag in energy-aware wakeups.
@@ -122,20 +122,20 @@ extern void ohm_schedstats_record(int sched_type, int fg, u64 delta);
  *
  * (default SCHED_TUNABLESCALING_LOG = *(1+ilog(ncpus))
  */
-enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LOG;
+enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
  *
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity		= 500000ULL;
-unsigned int normalized_sysctl_sched_min_granularity	= 500000ULL;
+unsigned int sysctl_sched_min_granularity		= (1000 / HZ) * 100000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= (1000 / HZ) * 100000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 6;
+static unsigned int sched_nr_latency = 10;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
