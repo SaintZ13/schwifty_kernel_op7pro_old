fb14272ef30cb974c0a0e6367be4f5c04049ae31
SaintZ13 <spaceghostpurp93@gmail.com>
2020-09-27 16:42:47 -0400

    schwifty: just some building issues fixed

--- build_schwifty.sh
+++ build_schwifty.sh
@@ -20,8 +20,8 @@ export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 # export CROSS_COMPILE=~/gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 # export CROSS_COMPILE_ARM32=~/gcc/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export CROSS_COMPILE=~/gcc2/arm64-gcc/bin/aarch64-elf-
-export CROSS_COMPILE_ARM32=~/gcc2/arm32-gcc/bin/arm-eabi-
+export CROSS_COMPILE=~/gcc/arm64-gcc/bin/aarch64-elf-
+export CROSS_COMPILE_ARM32=~/gcc/arm32-gcc/bin/arm-eabi-
 export LD_LIBRARY_PATH=~/tc-build/install/lib:$LD_LIBRARY_PATH
 
 echo
--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -557,8 +557,6 @@ bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 	hdr = module_alloc(size);
 	if (!hdr) {
 		bpf_jit_uncharge_modmem(pages);
-#ifdef CONFIG_MODULES
-	hdr = module_alloc(size);
 #else
 	hdr = vmalloc_exec(size);
 #endif
@@ -586,8 +584,6 @@ void bpf_jit_binary_free(struct bpf_binary_header *hdr)
 #ifdef CONFIG_MODULES
 	module_memfree(hdr);
 	bpf_jit_uncharge_modmem(pages);
-#ifdef CONFIG_MODULES
-	module_memfree(hdr);
 #else
 	vfree(hdr);
 #endif
