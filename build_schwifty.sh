9ca0aad4584ae1c32ad792cc4c87c566ba5df076
SaintZ13 <spaceghostpurp93@gmail.com>
2020-10-04 00:55:12 -0400

    schwifty: makefile and build changes

--- build_schwifty.sh
+++ build_schwifty.sh
@@ -18,10 +18,10 @@ export SUBARCH=arm64
 export CLANG_PATH=~/tc-build/install/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-# export CROSS_COMPILE=~/gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-# export CROSS_COMPILE_ARM32=~/gcc/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export CROSS_COMPILE=~/gcc/arm64-gcc/bin/aarch64-elf-
-export CROSS_COMPILE_ARM32=~/gcc/arm32-gcc/bin/arm-eabi-
+export CROSS_COMPILE=~/gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=~/gcc/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=~/gcc/arm64-gcc/bin/aarch64-elf-
+#export CROSS_COMPILE_ARM32=~/gcc/arm32-gcc/bin/arm-eabi-
 export LD_LIBRARY_PATH=~/tc-build/install/lib:$LD_LIBRARY_PATH
 
 echo
